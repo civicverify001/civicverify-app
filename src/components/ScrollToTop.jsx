f189cdfc418719f63219b258945d1aa5d56dfe57
civicverify001 <civicverify001@gmail.com>
2026-02-25 15:07:29 -0500

    Update ScrollToTop.jsx

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -3,4 +3,8 @@ import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
   var location = useLocation();
-  useEffect(
+  useEffect(function () {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+  return null;
+}
