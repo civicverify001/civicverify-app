2bec74026c8b47258730b35f45dc8f7816487e01
civicverify001 <civicverify001@gmail.com>
2026-02-25 23:12:31 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.css
+import './styles/mobile-overflow-fix.css';
 // Global styles
 const style = document.createElement('style')
 style.textContent = `
