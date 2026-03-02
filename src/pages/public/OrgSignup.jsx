015aa2f0b4f9373f6852ae1d584e77dbd6ad9738
civicverify001 <civicverify001@gmail.com>
2026-03-02 12:46:01 -0500

    Update OrgSignup.jsx

--- src/pages/public/OrgSignup.jsx
+++ src/pages/public/OrgSignup.jsx
@@ -1,3 +1,7 @@
+import CanonicalUrl from '../../components/CanonicalUrl'
+
+// Inside return(), first line:
+<CanonicalUrl />
 // src/pages/public/OrgSignup.jsx
 import { useState, useRef } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
